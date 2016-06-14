69319a9fc5cc2cb32d09df41390e31087ba4f0e7
danielgwilson <daniel@danielgwilson.com>
2016-06-14 15:45:31 -0400

    Changed to USDA data with participation rates by individual; 2010 now.

--- counties.R
+++ counties.R
@@ -1,37 +1,30 @@
 # County Data
-setwd("~/Google Drive/Booz/Counties")
+setwd("~/Google Drive/Booz/FoodStampMafia")
 raw_FoodStamps <- read.csv("ACS_14_5YR_S2201_with_ann.csv", stringsAsFactors = FALSE)
 raw_FoodStamps <- raw_FoodStamps[-1,]
-raw_Poverty <- read.csv("ACS_14_5YR_C17002_with_ann.csv", stringsAsFactors = FALSE)
+raw_Poverty <- read.csv("ACS_10_5YR_S1703_with_ann.csv", stringsAsFactors = FALSE)
 raw_Poverty <- raw_Poverty[-1,]
+raw_SNAP <- read.csv("snap_data.csv", stringsAsFactors = FALSE)
 
 
 ##############################
 # ----------------------------
 # build main model dataset
 model <- data.frame(matrix(nrow = 3142))
-model$GEO.id <- raw_FoodStamps$GEO.id2
+model$GEO.id2 <- raw_FoodStamps$GEO.id2
+model[1] <- NULL
 model$County <- raw_FoodStamps$GEO.display.label
 
 # state filter
 model$State <- sub(' ', '', sapply(model$County, FUN = function(x) {strsplit(x, split = ",")[[1]][2]}))
 model$County <- sapply(model$County, FUN = function(x) {strsplit(x, split = ",")[[1]][1]})
 
-model$TotalHouseholds <- raw_FoodStamps$HC01_EST_VC01
-model$SNAP_Households <- raw_FoodStamps$HC02_EST_VC01
-model$Poverty_Sub100 <- as.numeric(raw_Poverty$HD01_VD02) + as.numeric(raw_Poverty$HD01_VD03)
-model[1] <- NULL
+model <- merge(model, data.frame(
+  GEO.id2 = raw_Poverty$GEO.id2, 
+  Poverty_Sub125 = (as.numeric(raw_Poverty$HC04_EST_VC01) / 100.0) * as.numeric(raw_Poverty$HC01_EST_VC01)), 
+  by.x = 1,
+  by.y = 1)
 
 
 # engineered features
-model$SNAP_ParticipationRate <- as.numeric(model$SNAP_Households) / as.numeric(model$TotalHouseholds)
-model$SNAP_FillRate <- as.numeric(model$SNAP_Households) / as.numeric(model$Poverty_Sub100)
 
-
-# county with a specific SNAP_Participation_Rate
-countyWithRatio <- function(x) {
-  result <- model$County[findInterval(x, model$SNAP_ParticipationRate[order(as.factor(model$SNAP_ParticipationRate))])]
-  return(result)
-}
-
-countyWithRatio(0.5278107)
