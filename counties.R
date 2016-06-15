dfb6e2be4e1ca26f45fb5a4558ee03eb2c571514
danielgwilson <daniel@danielgwilson.com>
2016-06-15 10:24:34 -0400

    Test commit to check Elaine's functionality

--- counties.R
+++ counties.R
@@ -1,5 +1,4 @@
 # County Data
-setwd("~/Google Drive/Booz/FoodStampMafia")
 raw_FoodStamps <- read.csv("ACS_14_5YR_S2201_with_ann.csv", stringsAsFactors = FALSE)
 raw_FoodStamps <- raw_FoodStamps[-1,]
 raw_Poverty <- read.csv("ACS_10_5YR_S1703_with_ann.csv", stringsAsFactors = FALSE)
@@ -35,3 +34,28 @@ model <- merge(model, data.frame(
 
 model$SNAP_Recipients <- sub(',', '', as.character(model$SNAP_Recipients))
 model$SNAP_ParticipationRatio <- as.numeric(model$SNAP_Recipients) / as.numeric(model$Poverty_Sub125)
+model <- merge(model, data.frame(
+  GEO.id2 = raw_SNAP$FIPS,
+  Population = raw_SNAP$POP10),
+  by.x = 1,
+  by.y = 1)
+model$Population <- sub(',', '', as.character(model$Population))
+
+# split into training and test sets
+smpl_size <- floor(0.75 * nrow(model))
+
+set.seed(1337)
+train_indeces <- sample(seq_len(nrow(model)), size = smpl_size)
+
+train <- model[train_indeces, ]
+test <- model[-train_indeces, ]
+
+
+# random forest
+library(rpart)
+library(randomForest)
+fit <- randomForest(as.factor(SNAP_ParticipationRatio) ~ as.factor(State) + as.factor(Population),
+                   data=train,
+                   importance=TRUE,
+                   ntree=2000)
+varImpPlot(fit)
