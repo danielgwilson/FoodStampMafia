0510bad9e99bd99e575edb9f1fb4e589257575a1
dweingarten <weingarten_danielle@bah.com>
2016-06-15 14:31:41 -0400

    Added ratios.csv
    Divided SNAP_Under18 column by SNAP_Recipients, TotalChildren_Under18,
    and Population columns

--- Danielle.R
+++ Danielle.R
@@ -24,6 +24,7 @@ model <- merge(model, data.frame(
 model$Population <- as.numeric(gsub(',', '', as.character(model$Population)))
 summary(model$Population)
 
+#Total Children under 18 
 model <- merge(model, 
                data.frame(
                  FIPS = raw_AgeSex$GEO.id2,
@@ -39,6 +40,7 @@ model <- merge(model, data.frame(
   by.x = 1,
   by.y = 1)
 
+# SNAP households with children under 18
 model <- merge(model,
                data.frame(
                  FIPS = raw_FoodStamps$GEO.id2,
@@ -48,6 +50,31 @@ model <- merge(model,
                by.y = 1
 )
 
+# % of SNAP households that are under 18 
+model$SNAP_Recipients <- sub(',', '', model$SNAP_Recipients)
+model <- merge(model,data.frame(
+  FIPS = model$FIPS,
+  SNAP_Under18Ratio = as.numeric(model$SNAP_Under18) / as.numeric(model$SNAP_Recipients)
+    ),
+    by.x = 1,
+    by.y = 1
+)
 
+# SNAP households under 18 divided by Total Population that is under 18
+model <- merge(model,data.frame(
+  FIPS = model$FIPS,
+  SNAP_Under18PopRatio = (as.numeric(model$SNAP_Under18) / as.numeric(model$TotalChildren_Under18)
+  )),
+  by.x = 1,
+  by.y = 1
+)
 
+# SNAP households under 18 divided Population of counties
+model <- merge(model,data.frame(
+  FIPS = model$FIPS,
+  SNAP_Under18TotalPopRatio = (as.numeric(model$SNAP_Under18) / as.numeric(model$Population))
+  ),
+  by.x = 1,
+  by.y = 1
+)
 
