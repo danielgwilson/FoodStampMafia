a761120c2574b7d1ae537ebf3114c2a2eeb09938
danielgwilson <daniel@danielgwilson.com>
2016-06-15 10:41:27 -0400

    Added custom model saving to *name*.csv

--- counties.R
+++ counties.R
@@ -1,4 +1,6 @@
 # County Data
+setwd("~/Google Drive/Booz/FoodStampMafia")
+rm(list=ls())
 raw_FoodStamps <- read.csv("ACS_14_5YR_S2201_with_ann.csv", stringsAsFactors = FALSE)
 raw_FoodStamps <- raw_FoodStamps[-1,]
 raw_Poverty <- read.csv("ACS_10_5YR_S1703_with_ann.csv", stringsAsFactors = FALSE)
@@ -10,7 +12,7 @@ raw_SNAP <- read.csv("snap_data.csv", stringsAsFactors = FALSE)
 # ----------------------------
 # build main model dataset
 model <- data.frame(matrix(nrow = 3142))
-model$GEO.id2 <- raw_FoodStamps$GEO.id2
+model$FIPS <- raw_FoodStamps$GEO.id2
 model[1] <- NULL
 model$County <- raw_FoodStamps$GEO.display.label
 
@@ -19,15 +21,15 @@ model$State <- sub(' ', '', sapply(model$County, FUN = function(x) {strsplit(x, 
 model$County <- sapply(model$County, FUN = function(x) {strsplit(x, split = ",")[[1]][1]})
 
 model <- merge(model, data.frame(
-  GEO.id2 = raw_Poverty$GEO.id2, 
+  FIPS = raw_Poverty$GEO.id2, 
   Poverty_Sub125 = (as.numeric(raw_Poverty$HC04_EST_VC01) / 100.0) * as.numeric(raw_Poverty$HC01_EST_VC01)), 
   by.x = 1,
   by.y = 1)
 
-model$GEO.id2 = as.numeric(model$GEO.id2)
+model$FIPS = as.numeric(model$GEO.id2)
 
 model <- merge(model, data.frame(
-  GEO.id2 = raw_SNAP$FIPS,
+  FIPS = raw_SNAP$FIPS,
   SNAP_Recipients = raw_SNAP$PRGNUM10),
   by.x = 1,
   by.y = 1)
@@ -35,7 +37,7 @@ model <- merge(model, data.frame(
 model$SNAP_Recipients <- sub(',', '', as.character(model$SNAP_Recipients))
 model$SNAP_ParticipationRatio <- as.numeric(model$SNAP_Recipients) / as.numeric(model$Poverty_Sub125)
 model <- merge(model, data.frame(
-  GEO.id2 = raw_SNAP$FIPS,
+  FIPS = raw_SNAP$FIPS,
   Population = raw_SNAP$POP10),
   by.x = 1,
   by.y = 1)
@@ -49,6 +51,8 @@ train_indeces <- sample(seq_len(nrow(model)), size = smpl_size)
 
 train <- model[train_indeces, ]
 test <- model[-train_indeces, ]
+
+write.csv(model, file = "daniel.csv", row.names = FALSE)
 
 
 # random forest
