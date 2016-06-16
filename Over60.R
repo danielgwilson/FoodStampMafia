c2251deda3fb238799d014b406a0a713669bcbfa
dweingarten <weingarten_danielle@bah.com>
2016-06-16 15:03:51 -0400

    Added file.csv
    Added file of Food Stamp info from 2010 and made changes to Over60.R

--- Over60.R
+++ Over60.R
@@ -1,13 +1,15 @@
 setwd("~/GitHub/FoodStampMafia")
 rm(list=ls())
-raw_FoodStamps <- read.csv("ACS_14_5YR_S2201_with_ann.csv", stringsAsFactors = FALSE)
+raw_FoodStamps <- read.csv("ACS_10_5YR_S2201_with_ann.csv", stringsAsFactors = FALSE)
 raw_FoodStamps <- raw_FoodStamps[-1,]
 raw_AgeSex <- read.csv("ACS_10_5YR_S0101_with_ann.csv", stringsAsFactors = FALSE)
 raw_AgeSex <- raw_AgeSex[-1,]
-raw_SNAP <- read.csv("snap_data.csv", stringsAsFactors = FALSE)
+raw_SNAP <- read.csv("daniel.csv", stringsAsFactors = FALSE)
+raw_Poverty <- read.csv("ACS_10_5YR_S1703_with_ann.csv", stringsAsFactors = FALSE)
+raw_Poverty <- raw_Poverty[-1,]
 
 # build main model dataset
-model <- data.frame(matrix(nrow = 3142))
+model <- data.frame(matrix(nrow = 3143))
 model$FIPS <- raw_FoodStamps$GEO.id2
 model[1] <- NULL
 model$County <- raw_FoodStamps$GEO.display.label
@@ -17,11 +19,25 @@ model$County <- sapply(model$County, FUN = function(x) {strsplit(x, split = ",")
 
 model <- merge(model, data.frame(
   FIPS = raw_SNAP$FIPS,
-  Population = raw_SNAP$POP10),
+  Population = raw_SNAP$Population),
   by.x = 1,
   by.y = 1)
 model$Population <- as.numeric(gsub(',', '', as.character(model$Population)))
 summary(model$Population)
+
+model <- merge(model, data.frame(
+  FIPS = raw_SNAP$FIPS,
+  SNAP_Eligible = raw_SNAP$Poverty_Sub125),
+  by.x = 1,
+  by.y = 1
+)
+
+model <- merge(model, data.frame(
+  FIPS = raw_SNAP$FIPS,
+  SNAP_Recipients= raw_SNAP$SNAP_Recipients),
+  by.x = 1,
+  by.y = 1
+)
 
 model <- merge(model,
                data.frame(
@@ -40,4 +56,12 @@ model <- merge(model, data.frame(
   by.y = 1
   )
 
+# People over 65 years old that are less than 125% of the poverty level
+# HC01_EST_VC10 is the total amount of people who are over 65 years and in poverty 
+model <- merge(model, data.frame(
+  FIPS = raw_Poverty$GEO.id2,
+  SNAPEligible_Over60 = as.numeric(raw_Poverty$HC04_EST_VC10) / 100 * as.numeric(raw_Poverty$HC01_EST_VC10)),
+  by.x = 1,
+  by.y = 1
+)
 
